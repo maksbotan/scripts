2694ec3d01cf35f3ef61f536f336c2e2964fcad7
Maxim <kolmax94@gmail.com>
2011-01-03 01:01:14 +0300

    Python3-fication of analyze

--- analyze.py
+++ analyze.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 
+from __future__ import print_function, unicode_literals
+
 import sys, os, datetime, json
 
 if len(sys.argv) == 2:
     logfile = sys.argv[1]
 else:
-    logfile = '/home/maksotan/log.json'
+    logfile = '/home/maksbotan/log.json'
 
 if not os.path.exists(logfile):
-    print 'Logfile %s not found' % logfile
+    print ('Logfile %s not found' % logfile)
     sys.exit(1)
     
 def parse_time(time):
     t = [ int(i) for i in time.split('.')[0].split(':') ]
     return datetime.timedelta(hours=t[0], minutes=t[1], seconds=t[2])
 
-log = [ [unicode(i['name']), parse_time(i['online']), i['status'] ] for i in json.load(open(logfile, 'r')) ]
+log = [ [i['name'], parse_time(i['online']), i['status'] ] for i in json.load(open(logfile, 'r'), encoding='utf-8') ]
 log.sort(key=lambda l: l[1])
 log.reverse()
 
 for i in log:
-    print "%s\t%s\t%s" % tuple([ unicode(j).encode('utf-8') for j in i ])
+    print('{0:30}{1} {2}'.format(*[ j for j in i ]))
