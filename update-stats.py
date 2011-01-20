6c3b6f315e106d406219964e51604a80cb157487
Maxim <kolmax94@gmail.com>
2011-01-20 20:16:25 +0300

    Source beautifying

--- update-stats.py
+++ update-stats.py
@@ -11,7 +11,7 @@ log_regex = re.compile(r'log.json-(\d{4}-\d{2}-\d{2})') #Regex for finding valid
 
 basedir = os.environ.get('HOME', '/home/maksbotan') #Dir with logs
 #Get all log files
-logfiles = [ f for f in os.listdir(basedir) if log_regex.match(f) ]
+logfiles = [f for f in os.listdir(basedir) if log_regex.match(f)]
 
 logs = []
 for logfile in logfiles:
@@ -19,7 +19,7 @@ for logfile in logfiles:
         #Store ligfile in list along with it's date
         log = analyze.make_log(os.path.join(basedir, logfile))
         for i in xrange(len(log)):
-            log[i][1] = str(log[i][1]) #Make human-readable view of time
+            log[i][1] = str(log[i][1])  #Make human-readable view of time
         logs.append({'log': log, 'date': log_regex.match(logfile).group(1)})
 
 with open('stats.html', 'w') as f:
