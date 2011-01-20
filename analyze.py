78f340528ca1aeb69ff2d955ac6c9299a765283a
Maxim <kolmax94@gmail.com>
2011-01-20 18:21:30 +0300

    Little revork in analyze.py

--- analyze.py
+++ analyze.py
@@ -4,22 +4,27 @@ from __future__ import print_function, unicode_literals
 
 import sys, os, datetime, json
 
-if len(sys.argv) == 2:
-    logfile = sys.argv[1]
-else:
-    logfile = '/home/maksbotan/log.json'
-
-if not os.path.exists(logfile):
-    print ('Logfile %s not found' % logfile)
-    sys.exit(1)
-    
 def parse_time(time):
     t = [ int(i) for i in time.split('.')[0].split(':') ]
     return datetime.timedelta(hours=t[0], minutes=t[1], seconds=t[2])
 
-log = [ [i['name'], parse_time(i['online']), i['status'] ] for i in json.load(open(logfile, 'r'), encoding='utf-8') ]
-log.sort(key=lambda l: l[1])
-log.reverse()
+def make_log(logfile):
+    log = [ [i['name'], parse_time(i['online']), i['status'] ] for i in json.load(open(logfile, 'r'), encoding='utf-8') ]
+    log.sort(key=lambda l: l[1])
+    log.reverse()
 
-for i in log:
-    print('{0:30}{1} {2}'.format(*[ j for j in i ]))
+    return log
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        logfile = sys.argv[1]
+    else:
+        logfile = '/home/maksbotan/log.json'
+    if not os.path.exists(logfile):
+        print ('Logfile %s not found' % logfile)
+        sys.exit(1)
+  
+    log = make_log(logfile)
+
+    for i in log:
+        print('{0:30}{1} {2}'.format(*[ j for j in i ]))
